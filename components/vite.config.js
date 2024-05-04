288931ea6c93fea91ab9cc418aa90469b090e324
Nathan Petrangelo <nathan@petrangelo.org>
2024-05-03 23:20:24 -0700

    Add Vite

---
+++ components/vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+
+const mode = process.env.NODE_ENV || 'development';
+const prod = mode === 'production';
+
+export default defineConfig({
+    plugins: [
+        svelte({
+            /* plugin options */
+            compilerOptions: {
+                customElement: true,
+                dev: !prod
+            }
+        })
+    ],
+    build: {
+        lib: {
+            entry: './src/main.js',
+            name: 'svelte-app'
+        },
+        outDir: 'public'
+    }
+});
