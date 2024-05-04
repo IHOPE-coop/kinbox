b9a52b0bd9f61218674d11fe6facfc7591555d5d
Nathan Petrangelo <nathan@petrangelo.org>
2024-05-04 01:44:52 -0700

    Vite web components rendering from Rust server

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,19 @@ use maud::{DOCTYPE, html, Markup};
 async fn main() {
     let router = Router::new()
         .route("/", get(|| async { Html(svelteTest().await) }))
-        .route("/favicon.png", get(|| async { /* TODO: Image handler */} ))
-        .route("/styles.css", get(|| async { Css(getCss().await) }))
-        .route("/bundle.js", get(|| async { JavaScript(getJsBundle().await) }));
+        // .route("/favicon.png", get(|| async { /* TODO: Image handler */} ))
+        .route("/style.css", get(|| async { Css(getCss().await) }))
+        .route("/components.js", get(|| async { JavaScript(getJsBundle().await) }));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, router).await.unwrap();
 }
 
 async fn getCss() -> String {
-    tokio::fs::read_to_string("components/public/build/styles.css").await.expect("file should exist")
+    tokio::fs::read_to_string("components/dist/style.css").await.expect("file should exist")
 }
 
 async fn getJsBundle() -> String {
-    tokio::fs::read_to_string("components/public/build/bundle.js").await.expect("file should exist")
+    tokio::fs::read_to_string("components/dist/components.js").await.expect("file should exist")
 }
 
 async fn svelteTest() -> Markup {
@@ -28,11 +28,11 @@ async fn svelteTest() -> Markup {
         head {
             title { "Kinbox" }
             link href="favicon.png" rel="icon" type="image/png";
-            link href="bundle.css" rel="stylesheet";
-            script defer src="bundle.js" { }
+            link href="style.css" rel="stylesheet";
+            script defer type="module" src="components.js" { }
         }
         body {
-            my-element name="kinbox" { }
+            my-element name="Rust" { }
         }
     }
 }
