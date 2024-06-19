c5d9a2e3d446117d23144d9df6425b8c817d19ab
Nathan Petrangelo <nathan@petrangelo.org>
2024-06-19 00:49:25 -0700

    Add dummy data response stubs for all htmx calls

--- components/dummy-server.ts
+++ components/dummy-server.ts
@@ -3,8 +3,20 @@ import express, { Request, Response } from 'express';
 const app = express();
 
 // Example middleware
-app.use('/api/custom-endpoint', (req: Request, res: Response) => {
+app.get('/api/custom-endpoint', (req: Request, res: Response) => {
   res.json({ message: 'Hello from custom endpoint!' });
+});
+
+app.get('/hx-needs/:username', (req: Request, res: Response) => {
+  res.send('Needs response')
+});
+
+app.get('/hx-notifs/:username', (req: Request, res: Response) => {
+  res.send('Notifications response')
+});
+
+app.get('/hx-ledger/:username', (req: Request, res: Response) => {
+  res.send('Ledger response')
 });
 
 export default app;
