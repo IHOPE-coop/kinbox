877933b275f392684c77239d0ecb1e59d0a5c8ce
Nathan Petrangelo <nathan@petrangelo.org>
2024-06-19 01:03:13 -0700

    Added htmlHeader middleware function

--- components/dummy-server.ts
+++ components/dummy-server.ts
@@ -1,22 +1,32 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 
 const app = express();
 
+const htmlHeader = function (req: Request, res: Response, next: NextFunction) {
+  res.html = (markup: string) => {
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+    res.send(markup);
+  };
+  next();
+}
+
+app.use(htmlHeader);
+
 // Example middleware
 app.get('/api/custom-endpoint', (req: Request, res: Response) => {
-  res.json({ message: 'Hello from custom endpoint!' });
+  res.html({ message: 'Hello from custom endpoint!' });
 });
 
 app.get('/hx-needs/:username', (req: Request, res: Response) => {
-  res.send('Needs response')
+  res.html('Needs response');
 });
 
 app.get('/hx-notifs/:username', (req: Request, res: Response) => {
-  res.send('Notifications response')
+  res.html('Notifications response');
 });
 
 app.get('/hx-ledger/:username', (req: Request, res: Response) => {
-  res.send('Ledger response')
+  res.html('Ledger response');
 });
 
 export default app;
